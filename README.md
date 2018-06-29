ea26a0390bbc2cec5fe8fb80f23b8887955f1869
Devin Mounts <devinmounts@gmail.com>
2018-06-29 14:19:15 -0700

    recreate WordSearch.Solution as WordCounter.Solution.

---
+++ README.md
@@ -0,0 +1,45 @@
+﻿# Word Counter
+
+#### Epicodus C#: Wk. 1 Independent Project, 06/29/2018
+
+#### By Devin Mounts
+
+## A console application that will allow the user to input a word and check how frequently that word appears in a given string.
+
+### Specs
+| Spec | Input | Output |
+| :-------------     | :------------- | :------------- |
+| **Program allows user to enter a word, and stores that word**| Input: "The" | Output: "The" |
+| **Program converts stored word to lower case letters.**| Input: "The" | Output: "the" |
+| **Program allows user to enter in a string, and saves that string** | Input: "The cat" | Output: "The cat" |
+| **Program converts stored string to lower case letters.**| Input: "The cat" | Output: "the cat" |
+| **Program splits lowercase string into array of lowercase words.**| Input: "the cat" | Output: {"the", "cat"} |
+| **Program loops through arary of words checking to see if lowercase test word is present.**| Input: "Test Word: The; Test string: "The cat" | Output: "Compare: "the" to Array {"the", "cat"}." |
+| **Program returns an integer value for the number of times the test word appears in the list.**| Input: "Test Word: "The"; Test String: "The cat" | Output: "1" |
+
+## Setup/Contribution Requirements
+
+1. Follow the instructions for downloading C#, .NET Core 1.1 and Mono from: https://www.learnhowtoprogram.com/c/getting-started-with-c/installing-c
+1. Clone the repo at https://github.com/devinmounts/WordSearch.Solution.git
+1. Make a new branch
+1. Open WordSearch.Solution.sln in text editor
+1. Commit and push your changes
+1. Create a PR
+
+## Technologies Used
+
+* .NET Core 1.1 SDK
+* .NET runtime
+* MS Test 1.1.11
+* Mono
+* Visual Studio
+
+## Links
+
+*
+
+## License
+
+This software is licensed under the MIT license.
+
+Copyright (c) 2018 **Devin Mountso**
---
+++ WordCounter/Models/RepeatCounter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WordCounter.Models
+{
+    public class RepeatCounter
+    {
+        private int _result;
+        private string _targetWord;
+        private string[] _comparePhrase;
+
+        public RepeatCounter()
+        {
+
+        }
+
+        public RepeatCounter(string targetWord = "")
+        {
+            _targetWord = targetWord;
+        }
+
+        public RepeatCounter(string targetWord = "", string phrase = "")
+        {
+            _targetWord = targetWord;
+            _comparePhrase = phrase.ToLower().Split(' ');
+        }
+
+
+
+        public void SetTargetWord(string newTargetWord)
+        {
+            _targetWord = newTargetWord.ToLower();
+        }
+        public string GetTargetWord()
+        {
+            return _targetWord;
+        }
+
+        public void SetSplitCompareString(string newComparePhrase)
+        {
+            _comparePhrase = newComparePhrase.ToLower().Split(' ');
+        }
+
+        public string[] GetComparePhrase()
+        {
+            return _comparePhrase;
+        }
+
+        public int CheckSplitPhrase(string targetWord, string targetPhrase)
+        {
+            SetTargetWord(targetWord);
+            string inputWord = GetTargetWord();
+            SetSplitCompareString(targetPhrase);
+            string[] inputPhrase = GetComparePhrase();
+            if (!inputPhrase.Contains(inputWord))
+            {
+                return _result = 0;
+            }
+            else
+            {
+                int i = 0;
+                foreach (string word in inputPhrase)
+                {
+                    if (inputWord == inputPhrase[i])
+                    {
+                        _result += 1;
+                    }
+                    i++;
+                }
+                return _result;
+            }
+        }
+        public int GetResult()
+        {
+            return _result;
+        }
+    }
+}
